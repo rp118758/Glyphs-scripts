5d980f7d196c7e3adb4a3866ab771b190351806b
rp118758 <hello@robertpratley.co.uk>
2015-11-30 09:48:06 +0000

    New Scripts - Preview
    Adds a script that returns selected text strings omitting characters
    with 0 paths (excluding space)

---
+++ Preview/remove empty glyphs from string.py
@@ -0,0 +1,25 @@
+#MenuTitle: Remove missing glyphs from sample string
+# -*- coding: utf-8 -*-
+__doc__="""Opens a new tab from selected sample text, removing glyphs that contain no paths (excluding word spaces)"""
+
+# -> Robert Pratley aka rp118758 @ GitHub
+# -> www.robertpratley.co.uk
+
+import GlyphsApp
+
+Doc = Glyphs.currentDocument
+Font = Glyphs.font
+selectedLayers = Font.selectedLayers
+entryText = [ thisLayer.parent.name for thisLayer in selectedLayers ]
+
+emptyGlyphs = []
+for thisLayer in selectedLayers:
+	numberOfPaths = len(thisLayer.paths)
+	if numberOfPaths < 1:
+		if thisLayer.parent.name != 'space':
+			emptyGlyphs.append(thisLayer.parent.name)
+			
+newText = [x for x in entryText if x not in emptyGlyphs]
+newText = "/" + "/ ".join(newText).replace(' ', '')
+
+callAfter( Doc.windowController().addTabWithString_, newText ) # opens new edit tab with the new string.
