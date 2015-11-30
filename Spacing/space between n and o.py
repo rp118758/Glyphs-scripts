598824dc5fbac168112d9676784c5c3eea7aa081
rp118758 <hello@robertpratley.co.uk>
2015-11-30 14:11:19 +0000

    update to spacing to test for button

--- Spacing/space between n and o.py
+++ Spacing/space between n and o.py
@@ -7,12 +7,30 @@ __doc__="""Opens a new tab from the selected character,creating a placeholder be
 
 
 import GlyphsApp
+import vanilla
 
 Doc = Glyphs.currentDocument
 Font = Glyphs.font
 selectedLayers = Font.selectedLayers
 entryText = [ thisLayer.parent.name for thisLayer in selectedLayers ]
 entryText = "/" + "/ ".join(entryText).replace(' ', '')
-newText = entryText + "\n/n/Placeholder/n/n/Placeholder/n/Placeholder/Placeholder/n\n/o/Placeholder/o/o/Placeholder/o/Placeholder/Placeholder/o"
 
-Glyphs.currentDocument.windowController().addTabWithString_( newText )
+newText = entryText + "\n/n/Placeholder/n/n/Placeholder/n/Placeholder/Placeholder/n\n/o/Placeholder/o/o/Placeholder/o/Placeholder/Placeholder/o"
+#newText = entryText + "\n/H/Placeholder/H/H/Placeholder/H/Placeholder/Placeholder/H\n/O/Placeholder/O/O/Placeholder/O/Placeholder/Placeholder/O"
+#newText = entryText + "\n/a/Placeholder/d/Placeholder/h/Placeholder/e/Placeholder/s/Placeholder/i/Placeholder/o/Placeholder/n/Placeholder"
+
+class dataPick(object):
+	def __init__(self):
+		self.w = vanilla.FloatingWindow((500,200))
+		self.w.textbox = TextBox((10,10,-10,40), 'Choose a spacing string:')
+		self.w.button = Button((10, 10, -10, 20), "n and o", callback=self.buttonCallback)
+        self.w.open()
+
+    def buttonCallback(self, sender):
+   
+        characters = self.w.newText.get()
+		Glyphs.currentDocument.windowController().addTabWithString_( characters )
+		self.w.close()
+
+dataPick()
+
